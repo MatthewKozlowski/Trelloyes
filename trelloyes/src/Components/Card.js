9b15e90aedb556f4bfe044fa9608dd2749983634
Matthew Kozlowski <maekozlowski@gmail.com>
2019-07-28 12:37:31 -0500

    Completed App

---
+++ trelloyes/src/Components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './card.css';
+
+function Card(props) {
+    return (
+        <div className="card">
+            <button type="button">delete</button>
+            <h3 className="cardTitle">{props.title}</h3>
+            <p className="cardContent">{props.content}</p>
+        </div>
+    )
+}
+
+export default Card;
